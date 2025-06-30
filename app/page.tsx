cee18202736f3ec4adb270d047093b30853e6c5d
ettoiadev <ettoiadev@gmail.com>
2025-06-29 21:12:41 -0300

    ajuste de margem cart-footer

--- app/page.tsx
+++ app/page.tsx
@@ -548,7 +548,7 @@ function HomePageContent() {
         </div>
 
         {/* Cardápio */}
-        <div className="px-4 py-4 space-y-4 pb-24">
+        <div className="px-4 py-4 space-y-4 pb-32">
           {/* Seção Pizzas */}
           <Card>
             <CardContent className="p-4">
